15038f6affc0c08097189f738353dcfe4ec68c03
Josh Lapham <josh@joshlapham.com>
2014-07-03 09:23:38 +1000

    Added current_user helper method for views; added welcome message to logged-in user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+    @current_user
+  end
+
+  # Make this method available in views
+  helper_method :current_user
+
   private
 
   def confirm_logged_in
