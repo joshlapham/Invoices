de0e3dc9700c5cf2791f247181f348560e76643d
Josh Lapham <josh@joshlapham.com>
2014-07-03 13:36:43 +1000

    Split invoices on front page into different sections depending on status.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController
     @current_user = User.find_by(email: session[:email])
     @clients = Client.all
     @invoices = Invoice.all
+    @draft_invoices = Invoice.where(:status => "draft").all
+    @sent_invoices = Invoice.where(:status => "sent").all
+    @paid_invoices = Invoice.where(:status => "paid").all
   end
 
 end
