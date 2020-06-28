51d9ee09fce3df1412aa0129eab5191847f41963
Josh Lapham <josh@joshlapham.com>
2020-06-28 15:49:21 +1000

    Fix jQuery datepicker UI import error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.ui.datepicker
+//= require jquery-ui
 //= require jquery_ujs
 //= require bootstrap.min
 //= require_tree .
