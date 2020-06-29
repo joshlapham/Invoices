67281abc51855f3292f3539e4a050198b4830f36
Josh Lapham <josh@joshlapham.com>
2020-06-29 13:42:20 +1000

    Update DB schema file; change types from integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529023439) do
+ActiveRecord::Schema.define(version: 20170529031138) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
