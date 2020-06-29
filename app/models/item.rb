61c71add4cfd533ebda07c36baf04ea60a54ee6e
Josh Lapham <josh@joshlapham.com>
2020-06-29 15:31:55 +1000

    Implement total_price_discount_applied method on Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,12 @@ class Item < ActiveRecord::Base
 
   # Method to calculate total price of item after discount
   def total_price
+    (self.unit_cost.to_f * self.quantity.to_f)
+  rescue NoMethodError => method
+    0
+  end
+
+  def total_price_discount_applied
     (self.unit_cost.to_f * self.quantity.to_f) * ((100 - self.discount.to_f) / 100.0)
   rescue NoMethodError => method
     0
