ebfb03596a86721aea92221e81f62ee533a2cf98
Josh Lapham <josh@joshlapham.com>
2020-06-29 13:37:59 +1000

    Add simple unit test to Clients controller spec file

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 RSpec.describe ClientsController, :type => :controller do
 
 end
+
+class ClientsTest < ActiveSupport::TestCase
+  test "should successfully create new client" do
+    client = Client.new
+    assert_not client.save
+  end
+end
