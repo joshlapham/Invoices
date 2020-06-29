f5831a804954aed660e61d50a41d7eab270a8e88
Josh Lapham <josh@joshlapham.com>
2020-06-29 15:30:03 +1000

    Update WickedPDF init exe path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,10 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
+
+  # Mac
   :exe_path => '/usr/local/bin/wkhtmltopdf'
+
+  # Linux
+  # :exe_path => '/usr/bin/wkhtmltopdf'
 }
