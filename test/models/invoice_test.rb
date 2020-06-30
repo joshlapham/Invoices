76e66235e6bdb5f5dff07faee10e97328680855e
Josh Lapham <josh@joshlapham.com>
2020-06-30 12:08:15 +1000

    Misc cleanup

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -8,7 +8,7 @@ class InvoiceTest < ActiveSupport::TestCase
     @invoice.items.create(description: "Test item", unit_cost: 120.0, quantity: 3.0)
 
     # TODO: [2020] This is failing -- actual is `396.0`, so GST is being applied
-    # assert_equal 360.0, @invoice.calculate_subtotal, "calculate_subtotal method failed to return expected value"
+    assert_equal 360.0, @invoice.calculate_subtotal, "calculate_subtotal method failed to return expected value"
 
     # TODO: [2020] This is a private method, so this fails
     # assert_equal 396.0, @invoice.calculate_total, "calculate_total method failed to return expected value"
