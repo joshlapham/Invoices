866c2a635fdb80e26805c16dee6babf4d6678cc3
Josh Lapham <josh@joshlapham.com>
2020-06-30 11:54:32 +1000

    Continue implementing unit tests for Item class

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -14,6 +14,21 @@ class ItemTest < ActiveSupport::TestCase
 
     assert_equal 360.0, @item.total_price, "total_price method on Item class failed to return expected value"
     assert_equal 180.0, @item.total_price_discount_applied, "total_price_discount_applied method on Item class failed to return expected value"
+
+    # TODO: This is failing -- `calculate_gst` method is not taking into account when a discount is applied
     assert_equal 18.0, @item.calculate_gst, "calculate_gst method on Item class failed to return expected value"
+  end
+
+  test "should correctly calculate total price with no discount applied on Item class" do
+    @item = Item.new
+    @item.description = "Test item"
+
+    # 120 x 3 = 360
+    @item.unit_cost = 120.0
+    @item.quantity = 3.0
+
+    assert_equal 360.0, @item.total_price, "total_price method on Item class failed to return expected value"
+    assert_equal 360.0, @item.total_price_discount_applied, "total_price_discount_applied method on Item class failed to return expected value"
+    assert_equal 36.0, @item.calculate_gst, "calculate_gst method on Item class failed to return expected value"
   end
 end
