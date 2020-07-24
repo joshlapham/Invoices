5d58a9853279ef3a07c3b34989acbed5a5ca0331
Josh Lapham <josh@joshlapham.com>
2020-07-24 12:05:11 +1000

    Add formatting methods for BSB and Account Number on PaymentDetails class

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -7,4 +7,14 @@ class PaymentDetail < ActiveRecord::Base
     # Returns a correctly formatted AU ABN as a String
     ActiveSupport::NumberHelper::number_to_delimited(self.abn_number, delimiter: " ")
   end
+
+  def bsb_number_formatted
+    # Returns a correctly formatted AU BSB as a String
+    ActiveSupport::NumberHelper::number_to_delimited(self.bsb_number, delimiter: " ")
+  end
+
+  def account_number_formatted
+    # Returns a correctly formatted AU Account Number as a String
+    ActiveSupport::NumberHelper::number_to_delimited(self.account_number, delimiter: " ")
+  end
 end
--- test/models/payment_detail_test.rb
+++ test/models/payment_detail_test.rb
@@ -43,4 +43,38 @@ class PaymentDetailTest < ActiveSupport::TestCase
     assert_equal details.abn_number_formatted, "12 345 678 910", "'abn_number_formatted' returned incorrect formatting"
   end
 
+  test "should correctly format 'bsb_number'" do
+    details = PaymentDetail.new
+
+    # Note: 6 digit BSB number for AU
+    # TODO: [2020] Enforce this if AU
+    details.bsb_number = 123456
+
+    # Note: 8 digit account number for AU
+    # TODO: [2020] Enforce this if AU
+    details.account_number = 12345678
+
+    # NOTE: 11 digits for AU ABN number
+    details.abn_number = 12345678910
+
+    assert_instance_of String, details.bsb_number_formatted, "'bsb_number_formatted' returned incorrect class type"
+    assert_equal details.bsb_number_formatted, "123 456", "'bsb_number_formatted' returned incorrect formatting"
+  end
+
+  test "should correctly format 'account_number'" do
+    details = PaymentDetail.new
+
+    # Note: 6 digit BSB number for AU
+    details.bsb_number = 123456
+
+    # Note: 8 digit account number for AU
+    details.account_number = 12345678
+
+    # NOTE: 11 digits for AU ABN number
+    details.abn_number = 12345678910
+
+    assert_instance_of String, details.account_number_formatted, "'account_number_formatted' returned incorrect class type"
+    assert_equal details.account_number_formatted, "12 345 678", "'account_number_formatted' returned incorrect formatting"
+  end
+
 end
