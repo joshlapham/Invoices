259164b6c3a41fb462c7a9aaace8521f7bcf9469
Josh Lapham <josh@joshlapham.com>
2020-07-24 11:12:00 +1000

    Update TODOs

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,5 +1,7 @@
 class PaymentDetail < ActiveRecord::Base
   belongs_to :user
   validates :bsb_number, :account_number, :abn_number, presence: true
+
+  # TODO: [feature/invoice-gst-toggle] `numericality` might not be correct to use here -- how do we handle formatting/spaces in numbers? in views?
   validates :bsb_number, :account_number, :abn_number, numericality: true
 end
