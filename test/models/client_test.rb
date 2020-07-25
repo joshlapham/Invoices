d39ed31a0467a4fa449a1b01c0d5995629068627
Josh Lapham <josh@joshlapham.com>
2020-07-26 09:22:15 +1000

    Test fixture data in Client test

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
+  test "Test fixture data" do
+    client = clients(:testclient)
+    assert_equal client.user.email, "test@user.com"
+    assert_equal client.name, "Test Client"
+  end
+
   test "should not save a new Client without 'name'" do
     @client = Client.new
 
