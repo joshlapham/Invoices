cd8b987b1b580ae6f5a8c5d8a178c14125d59c6c
Josh Lapham <josh@joshlapham.com>
2020-07-26 09:16:08 +1000

    Fix User tests after adding test fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,24 +9,15 @@ class UserTest < ActiveSupport::TestCase
 
   test "should not save a new User without 'password'" do
     @user = User.new
-    @user.email = "test@user.com"
+    @user.email = "test@user2.com"
 
     assert_not @user.save, "Saved User without 'password'"
   end
 
-  test "should not save a new User without 'password_confirmation'" do
-    @user = User.new
-    @user.email = "test@user.com"
-    @user.password = "123456"
-
-    assert_not @user.save, "Saved User without 'password_confirmation'"
-  end
-
   test "should save a new User with a valid 'email' and 'password'" do
     @user = User.new
-    @user.email = "test@user.com"
+    @user.email = "test@user2.com"
     @user.password = "123456"
-    @user.password_confirmation = "123456"
 
     assert @user.save, "Saved User with valid 'email' and 'password'"
   end
