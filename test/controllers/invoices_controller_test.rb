7864d111b9f4db0fc941ba174705cf668e2600ad
Josh Lapham <josh@joshlapham.com>
2020-07-26 08:45:04 +1000

    Add more asserts to controller tests

--- test/controllers/invoices_controller_test.rb
+++ test/controllers/invoices_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class InvoicesControllerTest < ActionDispatch::IntegrationTest
   test "should redirect to /login if not logged-in and accessing '/new' endpoint" do
     get new_invoice_url
+    assert_equal "new", @controller.action_name
     assert_redirected_to '/login'
+    assert_equal flash[:notice], "Please log in"
   end
 end
