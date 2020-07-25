77f4ed29cccecbd921287daf2f1386bcaffa7a02
Josh Lapham <josh@joshlapham.com>
2020-07-26 09:33:12 +1000

    Add Users controller test for successful login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,4 +5,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_redirected_to '/login'
   end
+
+  test "should redirect to main page on successful login" do
+    test_user = users(:testuser)
+    password = "password123"
+    post '/attempt_login', params: { email: test_user.email, password: password }
+    assert_equal @controller.action_name, "attempt_login"
+    assert_equal session[:user_id], test_user.id
+    assert_equal session[:email], test_user.email
+    assert_equal flash[:notice], "You are now logged in"
+    assert_redirected_to '/'
+  end
 end
