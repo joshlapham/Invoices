8d589a6847b9b657ab1e5610920b3cefe3aba899
Josh Lapham <josh@joshlapham.com>
2020-07-27 08:10:53 +1000

    Continue writing test for ApplicationController

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test "'current_user' method should return nil" do
+  test "'current_user' method should return nil when not logged-in" do
     flunk("Write me!")
+    # ApplicationController::current_user
+    # assert_nil current_user
+    # assert_nil @controller.current_user
   end
 end
