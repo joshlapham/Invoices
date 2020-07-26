489ec164e8c3cdb393ac4777ea8178a6beee19a7
Josh Lapham <josh@joshlapham.com>
2020-07-26 18:41:37 +1000

    Misc cleanup on Invoices controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,9 +1,8 @@
 class InvoicesController < ApplicationController
-
-  before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
+  before_action :confirm_logged_in, except: [:login, :attempt_login, :logout]
 
   def show
-    # TODO - limit invoice search to current_user
+    # TODO: limit invoice search to current_user
     @invoice = Invoice.find_by(id: params[:id])
     @items = @invoice.items
     @client_this_belongs_to = Client.find_by(id: @invoice.client_id)
@@ -11,13 +10,13 @@ class InvoicesController < ApplicationController
 
     respond_to do |format|
       format.pdf do
-        render :pdf    => "Invoice #{@invoice.invoice_number}",
-          :disposition => "inline",
-          :template    => "invoices/show.pdf.erb",
-          :show_as_html => params[:debug].present?,
-          :disable_external_links => true,
-          :orientation => 'Portrait',
-          :layout => "pdf_layout.html.erb"
+        render pdf: "Invoice #{@invoice.invoice_number}",
+               disposition: 'inline',
+               template: 'invoices/show.pdf.erb',
+               show_as_html: params[:debug].present?,
+               disable_external_links: true,
+               orientation: 'Portrait',
+               layout: 'pdf_layout.html.erb'
       end
 
       format.html
@@ -34,8 +33,8 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.new(invoice_params)
 
     if @invoice.save
-      flash[:notice] = "Invoice saved successfully"
-      redirect_to(:controller => 'users', :action => 'index')
+      flash[:notice] = 'Invoice saved successfully'
+      redirect_to(controller: 'users', action: 'index')
     else
       render('new')
     end
@@ -51,8 +50,8 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.find_by(id: params[:id])
 
     if @invoice.update_attributes(invoice_params)
-      flash[:notice] = "Invoice updated successfully"
-      redirect_to(:controller => 'invoices', :action => 'show', :id => @invoice.id)
+      flash[:notice] = 'Invoice updated successfully'
+      redirect_to(controller: 'invoices', action: 'show', id: @invoice.id)
     else
       render('edit')
     end
@@ -67,12 +66,11 @@ class InvoicesController < ApplicationController
     invoice = Invoice.find_by(id: params[:id])
     invoice.destroy
     flash[:notice] = "Invoice '#{invoice.invoice_number}' deleted"
-    redirect_to(:controller => 'users', :action => 'index')
+    redirect_to(controller: 'users', action: 'index')
   end
 
   private
-  # Note -
-  # - allows listed attribs to be mass-assigned
+
   def invoice_params
     params.require(:invoice).permit(:id, :invoice_number, :client_id, :amount, :date_sent, :date_due, :status, :should_charge_gst, items_attributes: [:id, :description, :quantity, :unit_cost, :discount, :invoice_id, :_destroy])
   end
