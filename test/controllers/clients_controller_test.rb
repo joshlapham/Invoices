f6f91c709b193a731676272f251e10688da956b3
Josh Lapham <josh@joshlapham.com>
2020-07-26 12:08:56 +1000

    Write test for Clients controller

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -7,4 +7,15 @@ class ClientsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to '/login'
     assert_equal flash[:notice], "Please log in"
   end
+
+  test 'should show Client if logged-in' do
+    test_user = users(:testuser)
+    test_password = 'password123'
+    do_log_in(test_user, test_password)
+
+    test_client = clients(:testclient)
+
+    get client_url(test_client)
+    assert_response :success
+  end
 end
