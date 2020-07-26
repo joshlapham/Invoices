3e70c2ea251b1ae01ced12f45c020961d9bea6bc
Josh Lapham <josh@joshlapham.com>
2020-07-26 19:14:48 +1000

    Update User model tests to use Faker gem

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'faker'
 
 class UserTest < ActiveSupport::TestCase
   test "should not save a new User without 'email'" do
@@ -9,15 +10,15 @@ class UserTest < ActiveSupport::TestCase
 
   test "should not save a new User without 'password'" do
     @user = User.new
-    @user.email = "test@user2.com"
+    @user.email = Faker::Internet.unique.email
 
     assert_not @user.save, "Saved User without 'password'"
   end
 
   test "should save a new User with a valid 'email' and 'password'" do
     @user = User.new
-    @user.email = "test@user2.com"
-    @user.password = "123456"
+    @user.email = Faker::Internet.unique.email
+    @user.password = Faker::Internet.unique.password
 
     assert @user.save, "Saved User with valid 'email' and 'password'"
   end
