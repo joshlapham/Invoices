9d21c40b64ab83d55488241c3ade995e77174444
Josh Lapham <josh@joshlapham.com>
2020-07-26 18:50:21 +1000

    Implement Users controller test for checking front page elements upon login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -49,4 +49,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal flash[:notice], 'Invalid username or password'
     assert_redirected_to '/login'
   end
+
+  test "should have correct 'Client' objects for 'User' when redirected to main page on successful login" do
+    test_user = users(:testuser)
+    test_password = 'password123'
+    do_log_in(test_user, test_password)
+
+    assert_equal @controller.action_name, 'attempt_login'
+    assert_equal session[:user_id], test_user.id
+    assert_equal session[:email], test_user.email
+    assert_equal flash[:notice], 'You are now logged in'
+    assert_response :redirect
+    assert_redirected_to '/'
+    follow_redirect!
+    assert_response :success
+
+    # TODO: how to test `@clients` instance variable on `@controller`?
+    # puts @controller.instance_variables
+    # controller_clients = @controller.instance_variable_get(clients)
+    # controller_clients = @controller.instance_variables[:clients]
+
+    # Test elements on main page
+    assert_select 'h3', 'Clients'
+
+    # Ensure a link to test client exists
+    assert_select 'a', clients(:testclient).name
+  end
 end
