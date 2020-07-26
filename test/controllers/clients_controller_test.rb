917716d593a560ad6df4e8b803ef4b39fc35a1a4
Josh Lapham <josh@joshlapham.com>
2020-07-26 13:11:59 +1000

    Formatting

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class ClientsControllerTest < ActionDispatch::IntegrationTest
   test "should redirect to /login if not logged-in and accessing '/new' endpoint" do
     get new_client_url
-    assert_equal "new", @controller.action_name
+    assert_equal 'new', @controller.action_name
     assert_redirected_to '/login'
-    assert_equal flash[:notice], "Please log in"
+    assert_equal flash[:notice], 'Please log in'
   end
 
   test 'should show Client if logged-in' do
--- test/controllers/invoices_controller_test.rb
+++ test/controllers/invoices_controller_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class InvoicesControllerTest < ActionDispatch::IntegrationTest
   test "should redirect to /login if not logged-in and accessing '/new' endpoint" do
     get new_invoice_url
-    assert_equal "new", @controller.action_name
+    assert_equal 'new', @controller.action_name
     assert_redirected_to '/login'
-    assert_equal flash[:notice], "Please log in"
+    assert_equal flash[:notice], 'Please log in'
   end
 
   test 'should show Invoice if logged-in' do
