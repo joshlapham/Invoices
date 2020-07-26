579d57599fea9ca3016a17c5737086ac92576961
Josh Lapham <josh@joshlapham.com>
2020-07-26 13:01:42 +1000

    Formatting

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should redirect to /login if not logged-in and accessing root URL" do
+  test 'should redirect to /login if not logged-in and accessing root URL' do
     get root_url
     assert_redirected_to '/login'
   end
