03e35a9b6bdff193a3c30a96628cf9610b45627d
Josh Lapham <josh@joshlapham.com>
2020-07-26 13:04:46 +1000

    Formatting

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,45 +6,45 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to '/login'
   end
 
-  test "should redirect to main page on successful login" do
+  test 'should redirect to main page on successful login' do
     test_user = users(:testuser)
-    password = "password123"
+    password = 'password123'
     post '/attempt_login', params: { email: test_user.email, password: password }
-    assert_equal @controller.action_name, "attempt_login"
+    assert_equal @controller.action_name, 'attempt_login'
     assert_equal session[:user_id], test_user.id
     assert_equal session[:email], test_user.email
-    assert_equal flash[:notice], "You are now logged in"
+    assert_equal flash[:notice], 'You are now logged in'
     assert_redirected_to '/'
   end
 
-  test "should redirect to login page on successful logout" do
+  test 'should redirect to login page on successful logout' do
     # Do login
     test_user = users(:testuser)
-    password = "password123"
+    password = 'password123'
     post '/attempt_login', params: { email: test_user.email, password: password }
-    assert_equal @controller.action_name, "attempt_login"
+    assert_equal @controller.action_name, 'attempt_login'
     assert_equal session[:user_id], test_user.id
     assert_equal session[:email], test_user.email
-    assert_equal flash[:notice], "You are now logged in"
+    assert_equal flash[:notice], 'You are now logged in'
     assert_redirected_to '/'
 
     #  Do logout
     get logout_url
-    assert_equal @controller.action_name, "logout"
+    assert_equal @controller.action_name, 'logout'
     assert_nil session[:user_id]
     assert_nil session[:email]
-    assert_equal flash[:notice], "Logged out"
+    assert_equal flash[:notice], 'Logged out'
     assert_redirected_to '/login'
   end
 
-  test "should redirect to login page on unsuccessful login" do
+  test 'should redirect to login page on unsuccessful login' do
     test_user = users(:testuser)
-    password = "incorrectpassword123"
+    password = 'incorrectpassword123'
     post '/attempt_login', params: { email: test_user.email, password: password }
-    assert_equal @controller.action_name, "attempt_login"
+    assert_equal @controller.action_name, 'attempt_login'
     assert_nil session[:user_id]
     assert_nil session[:email]
-    assert_equal flash[:notice], "Invalid username or password"
+    assert_equal flash[:notice], 'Invalid username or password'
     assert_redirected_to '/login'
   end
 end
