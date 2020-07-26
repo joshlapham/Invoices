1d1b403a8641a5c1c689351f76b0d5347524de39
Josh Lapham <josh@joshlapham.com>
2020-07-26 19:23:55 +1000

    Implement PaymentDetail test for editing a detail

--- test/controllers/payment_details_controller_test.rb
+++ test/controllers/payment_details_controller_test.rb
@@ -19,4 +19,16 @@ class PaymentDetailsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal @controller.action_name, 'show'
   end
+
+  test 'should edit PaymentDetail if logged-in' do
+    test_user = users(:testuser)
+    test_password = 'password123'
+    do_log_in(test_user, test_password)
+
+    test_payment_detail = payment_details(:testpaymentdetails)
+
+    get edit_payment_detail_url(test_payment_detail)
+    assert_response :success
+    assert_equal @controller.action_name, 'edit'
+  end
 end
