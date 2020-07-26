4df63de722d7e0c69b75dd48fd97a3e9efdad78b
Josh Lapham <josh@joshlapham.com>
2020-07-26 18:44:45 +1000

    Misc cleanup on Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,25 @@
 class SessionsController < ApplicationController
+  before_action :confirm_logged_in, except: [:login, :attempt_login, :logout]
 
-  before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
-
-  def login
-  end
+  def login; end
 
   def attempt_login
     if params[:email].present? && params[:password].present?
-      found_user = User.where(:email => params[:email]).first
-
-      if found_user
-        authorized_user = found_user.authenticate(params[:password])
-      end
+      found_user = User.where(email: params[:email]).first
+      authorized_user = found_user.authenticate(params[:password]) if found_user
     end
 
     if authorized_user
       # Mark user as logged in
       session[:user_id] = authorized_user.id
       session[:email] = authorized_user.email
-      flash[:notice] = "You are now logged in"
+      flash[:notice] = 'You are now logged in'
 
-      redirect_to(:controller => 'users', :action => 'index')
+      redirect_to(controller: 'users', action: 'index')
     else
-      flash[:notice] = "Invalid username or password"
+      flash[:notice] = 'Invalid username or password'
 
-      redirect_to(:action => 'login')
+      redirect_to(action: 'login')
     end
   end
 
@@ -32,9 +27,9 @@ class SessionsController < ApplicationController
     # Mark user as logged out
     session[:user_id] = nil
     session[:email] = nil
-    flash[:notice] = "Logged out"
+    flash[:notice] = 'Logged out'
 
-    redirect_to(:action => 'login')
+    redirect_to(action: 'login')
   end
 
 end
